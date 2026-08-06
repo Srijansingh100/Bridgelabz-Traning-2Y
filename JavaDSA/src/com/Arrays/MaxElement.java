be959ecf13c90d4dca3edf07bf613711f0124a1f
Srijan singh <srijansingh942@gmail.com>
2026-08-06 09:24:28 +0530

    [Srijan] new code added

---
+++ JavaDSA/src/com/Arrays/MaxElement.java
@@ -0,0 +1,15 @@
+package com.Arrays;
+
+public class MaxElement {
+    public static void main(String[] args) {
+        int max = Integer.MIN_VALUE;
+        int arr[]={1,2,3,4,5,6,7};
+        for(int i = 0 ;i<arr.length;i++){
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+        }
+        System.out.println(max);
+    }
+}
