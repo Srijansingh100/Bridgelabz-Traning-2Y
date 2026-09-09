49c9cd01cd1e54943262526a0434024c13b1f671
Srijansingh100 <srijansingh942@gmail.com>
2026-09-09 09:08:49 +0530

    Time: 11 ms (66.52%), Space: 46.5 MB (91.35%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,47 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-
-        List<Integer> result = new ArrayList<>();
-
-        if (s.length() < p.length()) {
-            return result;
+        int window=p.length();
+        List<Integer> list=new ArrayList<>();
+        if(s.length()<window){
+            return list;
         }
+        int sArr[]=new int[26];
+        int pArr[]=new int[26];
+        for(int i = 0;i<window;i++){
+            int si=s.charAt(i)-'a';
+            int pi=p.charAt(i)-'a';
+            sArr[si]++;
+            pArr[pi]++;
 
-        int[] countP = new int[26];
-        int[] countWindow = new int[26];
-
-        // p ke characters count karo
-        for (char ch : p.toCharArray()) {
-            countP[ch - 'a']++;
         }
-
-        int windowSize = p.length();
-
-        // First window
-        for (int i = 0; i < windowSize; i++) {
-            countWindow[s.charAt(i) - 'a']++;
+        if(Arrays.equals(pArr,sArr)){
+            list.add(0);
         }
-
-        // Check first window
-        if (Arrays.equals(countP, countWindow)) {
-            result.add(0);
-        }
-
-        // Sliding window
-        for (int i = windowSize; i < s.length(); i++) {
-
-            // New character add
-            countWindow[s.charAt(i) - 'a']++;
-
-            // Old character remove
-            countWindow[s.charAt(i - windowSize) - 'a']--;
-
-            // Anagram found
-            if (Arrays.equals(countP, countWindow)) {
-                result.add(i - windowSize + 1);
+        for(int i = 1; i<=s.length()-window;i++){
+            int prev=s.charAt(i-1)-'a';
+            int next=s.charAt(i+window-1)-'a';
+            sArr[prev]--;
+            sArr[next]++;
+            if(Arrays.equals(sArr,pArr)){
+                list.add(i);
             }
         }
-
-        return result;
+        return list;
     }
 }
