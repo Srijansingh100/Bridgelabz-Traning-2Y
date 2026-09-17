700bf35c948b4bb2e57ed2c0e7fde94fa207f96a
Srijansingh100 <srijansingh942@gmail.com>
2026-09-17 08:06:42 +0530

    Time: 8 ms (8.74%), Space: 46.6 MB (72.9%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashMap<ListNode, Integer> map = new HashMap<>();
+
+        while (head != null) {
+            if (map.containsKey(head))
+                return true;
+
+            map.put(head, 1);
+            head = head.next;
+        }
+
+        return false;
+    }
+}
